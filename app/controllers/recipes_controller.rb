685eb148675b93fbdd99d869f40d4002946a6aa5
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-18 00:08:00 +0000

    refactor, and cleanout layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update] # run this line of code for show/edit/update before anything in the method below
+  
   
   def index
     @recipes = Recipe.all
   end
   
   def show
-  @recipe = Recipe.find(params[:id])
   end
   
   def new
@@ -24,12 +25,10 @@ class RecipesController < ApplicationController
 end
   
   def edit
-    @recipe = Recipe.find(params[:id])
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
-    if @recipe.save
+    if @recipe.update(recipe_params)
       flash[:success] = "Recipe was updated successfully"
       redirect_to recipe_path(@recipe)
     else
@@ -38,7 +37,7 @@ end
 end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
+    set_recipe
     @recipe.destroy
    flash[:success] = "Recipe deleted successfully"
     redirect_to recipes_path
@@ -46,6 +45,10 @@ end
 
   
   private
+  
+  def set_recipe
+     @recipe = Recipe.find(params[:id])
+  end
   
   def recipe_params
     params.require(:recipe).permit(:name, :description)
