3992c689bc3ee24e4399ab59af910a9a1cc35d45
Rehan Pochkhanawala <rehanp@msn.com>
2017-03-21 04:59:31 +0000

    Users/Login/Authentication/Admin controllers

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,7 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  
   
   def index
     @chefs = Chef.all
@@ -11,6 +14,7 @@ class ChefsController < ApplicationController
   def create
     @chef = Chef.new(chef_params)
     if @chef.save
+      session[:chef_id] = @chef.id
       flash[:success] = "Wecome #{@chef.name} to my Recipes App"
       redirect_to chef_path(@chef)
     else
@@ -19,15 +23,15 @@ class ChefsController < ApplicationController
   end
   
   def show
-    @chef = Chef.find(params[:id])
+    set_chef
   end
   
   def edit
-    @chef = Chef.find(params[:id])
+    set_chef
   end
   
   def update
-    @chef = Chef.find(params[:id])
+    set_chef
     if @chef.update(chef_params)
       flash[:success] = "You account was updated successfully"
       redirect_to @chef
@@ -37,7 +41,7 @@ class ChefsController < ApplicationController
   end
   
   def destroy
-    @chef = Chef.find(params[:id])
+    set_chef
     @chef.destroy
     flash[:danger] = "Chef and all associated recipes have been deleted"
     redirect_to chefs_path
@@ -48,5 +52,17 @@ private
 def chef_params
   params.require(:chef).permit(:name, :email, :password, :password_confirmation)
 end
+
+  def set_chef
+    @chef = Chef.find(params[:id])
+  end
+
+  def require_same_user
+    if current_chef != @chef
+      flash[:danger] = "You can only edit or delete your own account"
+      redirect_to chefs_path
+    end
+  end
+
 
 end
